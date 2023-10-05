f9ad39cac454d37029cc2eeec4a0757915037332
Ryan Lubke <ryan.lubke@oracle.com>
2023-10-05 15:30:46 -0700

    Integrate Coherence 23.09.

--- coherence/src/test/java/io/micronaut/coherence/event/EventsHelper.java
+++ coherence/src/test/java/io/micronaut/coherence/event/EventsHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2020-2023 Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -10,6 +10,7 @@ package io.micronaut.coherence.event;
 import java.util.Map;
 
 import com.tangosol.coherence.component.util.daemon.queueProcessor.service.grid.partitionedService.PartitionedCache;
+import com.tangosol.coherence.component.util.daemon.queueProcessor.service.grid.partitionedService.partitionedCache.Storage;
 import com.tangosol.coherence.component.util.safeService.SafeCacheService;
 import com.tangosol.net.CacheService;
 import com.tangosol.net.NamedMap;
@@ -25,22 +26,12 @@ final class EventsHelper {
     private EventsHelper() {
     }
 
-    public static int getTotalListenerCount(NamedMap<?, ?> map) {
-        PartitionedCache.Storage storage = getStorage(map);
-        return getKeyListenerCount(storage) + getListenerCount(storage);
-    }
-
     public static int getListenerCount(NamedMap<?, ?> map) {
-        PartitionedCache.Storage storage = getStorage(map);
+        Storage storage = getStorage(map);
         return getListenerCount(storage);
     }
 
-    public static int getKeyListenerCount(NamedMap<?, ?> map) {
-        PartitionedCache.Storage storage = getStorage(map);
-        return getKeyListenerCount(storage);
-    }
-
-    private static int getListenerCount(PartitionedCache.Storage storage) {
+    private static int getListenerCount(Storage storage) {
         if (storage == null) {
             return 0;
         }
@@ -48,15 +39,7 @@ final class EventsHelper {
         return map != null ? map.size() : 0;
     }
 
-    private static int getKeyListenerCount(PartitionedCache.Storage storage) {
-        if (storage == null) {
-            return 0;
-        }
-        Map<?, ?> map = storage.getKeyListenerMap();
-        return map != null ? map.size() : 0;
-    }
-
-    private static PartitionedCache.Storage getStorage(NamedMap<?, ?> map) {
+    private static Storage getStorage(NamedMap<?, ?> map) {
         CacheService service = map.getService();
         if (service instanceof SafeCacheService) {
             service = ((SafeCacheService) service).getRunningCacheService();
