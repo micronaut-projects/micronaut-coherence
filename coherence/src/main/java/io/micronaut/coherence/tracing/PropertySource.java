7d3e663ea88284dfa778be545697cffd15e9fa94
Ryan Lubke <ryan.lubke@oracle.com>
2025-04-07 15:25:17 -0700

    Add PropertySource implementation to pass Micronaut configuration properties for OpenTelemetry to the Coherence OpenTelemetry initialization.

--- coherence/src/main/java/io/micronaut/coherence/tracing/PropertySource.java
+++ coherence/src/main/java/io/micronaut/coherence/tracing/PropertySource.java
@@ -16,20 +16,37 @@
 package io.micronaut.coherence.tracing;
 
 import io.micronaut.coherence.CoherenceContext;
+
 import io.micronaut.context.ApplicationContext;
+
+import io.micronaut.core.naming.NameUtils;
+
 import io.micronaut.core.naming.conventions.StringConvention;
 
 import java.util.stream.Collectors;
+
 import java.util.Map;
 
+/**
+ * A {@link PropertySource} implementation that exposes Micronaut's configuration to
+ * the {@code Coherence} {@code OpenTelemetry} integration.
+ */
 public class PropertySource implements com.tangosol.internal.tracing.PropertySource {
     public Map<String, String> getProperties() {
-        ApplicationContext applicationContext = CoherenceContext.getApplicationContext();
-        Map<String, Object> micronautProps = applicationContext.getProperties("otel", StringConvention.RAW);
-
-        return micronautProps.entrySet().stream().collect(Collectors.toMap(
+        ApplicationContext  applicationContext = CoherenceContext.getApplicationContext();
+        Map<String, Object> micronautProps     = applicationContext.getProperties("otel", StringConvention.RAW);
+        Map<String, String> otelProps          = micronautProps.entrySet().stream().collect(Collectors.toMap(
             e -> "otel." + e.getKey(),
             e -> e.getValue().toString()
         ));
+
+        // we use NameUtils.hyphenate() to be consistent with how
+        // Micronaut populates the application name in {@code ApplicationConfiguration}
+        // used by micronaut-tracing
+        otelProps.put("otel.service.name", NameUtils.hyphenate(
+            applicationContext.getProperty("micronaut.application.name", String.class)
+                .orElse("micronaut.coherence")));
+
+        return otelProps;
     }
 }
